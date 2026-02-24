e282ef0c829f9918e96c8ed634cbf5192739ee4c
Sohaib Mohamed <sohaib.amhmd@gmail.com>
2026-02-24 21:07:14 +0100

    nvim: don't trim trailspace on save
    Signed-off-by: Sohaib Mohamed <sohaib.amhmd@gmail.com>

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -160,7 +160,7 @@ return {
       vim.api.nvim_create_autocmd("BufWritePre", {
         pattern = "*",
         callback = function()
-          require("mini.trailspace").trim()
+          -- require("mini.trailspace").trim()
         end,
       })
     end,
